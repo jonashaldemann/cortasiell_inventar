7204b1fae2bff2a0f6910d64bbc86cd70714ebd1
jonashaldemann <jonasha@users.noreply.github.com>
2026-07-23 21:49:36 +0200

    update

--- app.js
+++ app.js
@@ -172,32 +172,26 @@ if ("serviceWorker" in navigator) {
 
 async function synchronisieren() {
 
-    const daten = JSON.parse(
-        localStorage.getItem("cortasiell_inventar")
-    );
-
     const url =
         "https://script.google.com/macros/s/AKfycbwanogPjcWXjI8gvu8qkeU_9gk_AZ8bVK7zFcAPlCTMIdtntauhVXz8HPCRx711Jg2S/exec";
-
-    console.log(url);
 
     try {
 
         const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(daten)
+            method: "GET"
         });
 
-        const result = await response.text();
+        const text = await response.text();
 
-        alert("Synchronisiert: " + result);
+        console.log(text);
+
+        alert(text);
 
     } catch (error) {
 
-        alert("Fehler: " + error);
+        console.error(error);
+
+        alert(error);
 
     }
 
